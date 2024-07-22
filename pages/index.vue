9d32084924ae2d1337daf87cdedb407f78f10aed
GUO YANKE <hi@yankeguo.com>
2024-07-22 12:37:53 +0800

    feat(ui): refresh ui

--- pages/index.vue
+++ pages/index.vue
@@ -134,83 +134,90 @@ watch(localTimeUpdater, updateLocalTime, { immediate: true });
   </Html>
 
   <div class="m-0 h-screen flex flex-col items-center justify-evenly">
-    <div class="flex flex-col items-center justify-center gap-4">
-      <img
-        class="rounded-full w-32"
-        src="~/assets/avatar-cartoon.jpg"
-        alt="photo of me"
-      />
+    <div class="flex flex-col items-center justify-center gap-8">
+      <div class="flex flex-col items-center justify-center gap-4">
+        <img
+          class="rounded-full w-32"
+          src="~/assets/avatar-cartoon.jpg"
+          alt="photo of me"
+        />
 
-      <div class="flex flex-row justify-center items-baseline">
-        <span class="font-bold text-3xl">Yanke Guo</span>
-        <span class="ms-2 text-sm text-slate-600 dark:text-slate-400"
-          >({{ $t("pronouns") }})</span
+        <div class="flex flex-row justify-center items-baseline">
+          <span class="font-bold text-3xl">Yanke Guo</span>
+          <span class="ms-2 text-sm text-slate-600 dark:text-slate-400"
+            >({{ $t("pronouns") }})</span
+          >
+        </div>
+
+        <div
+          class="flex flex-row justify-center items-center text-sm text-slate-600 dark:text-slate-400"
         >
-      </div>
+          <div class="flex flex-row items-center">
+            <UIcon name="i-heroicons-map-pin"></UIcon>
+            <span class="ms-1">{{ $t("location") }}</span>
+          </div>
 
-      <div
-        class="flex flex-row justify-center items-center text-sm text-slate-600 dark:text-slate-400"
-      >
-        <div class="flex flex-row items-center">
-          <UIcon name="i-heroicons-map-pin"></UIcon>
-          <span class="ms-1">{{ $t("location") }}</span>
-        </div>
-
-        <div class="flex flex-row items-center ms-4">
-          <UIcon name="i-heroicons-clock"></UIcon>
-          <ClientOnly>
-            <span class="ms-1">{{ localTime }}</span>
-          </ClientOnly>
+          <div class="flex flex-row items-center ms-4">
+            <UIcon name="i-heroicons-clock"></UIcon>
+            <ClientOnly>
+              <span class="ms-1">{{ localTime }}</span>
+            </ClientOnly>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="flex flex-col items-center justify-center gap-4">
-      <template v-for="(group, groupIdx) in linksSocial" v-bind:key="groupIdx">
-        <div class="flex flex-row justify-center items-center">
-          <UButton
-            v-for="(item, idx) in group"
-            v-bind:key="idx + '.' + groupIdx"
-            variant="ghost"
-            :label="item.label"
-            :icon="item.icon"
-            :to="item.to"
-            target="_blank"
-          ></UButton>
-        </div>
-      </template>
+      <UDivider></UDivider>
 
-      <div class="flex flex-row justify-center items-center">
-        <UBadge class="me-2" variant="outline" color="orange">{{
-          $t("donation")
-        }}</UBadge>
-        <template v-for="(itemGroup, idxGroup) in linksDonations">
-          <UButton
-            color="orange"
-            v-for="(item, idx) in itemGroup"
-            v-bind:key="idx"
-            size="sm"
-            variant="ghost"
-            :label="item.label"
-            :icon="item.icon"
-            :to="item.to"
-            target="_blank"
-          ></UButton>
+      <div class="flex flex-col items-center justify-center gap-4">
+        <template
+          v-for="(group, groupIdx) in linksSocial"
+          v-bind:key="groupIdx"
+        >
+          <div class="flex flex-row justify-center items-center">
+            <UButton
+              v-for="(item, idx) in group"
+              v-bind:key="idx + '.' + groupIdx"
+              variant="ghost"
+              :label="item.label"
+              :icon="item.icon"
+              :to="item.to"
+              target="_blank"
+            ></UButton>
+          </div>
         </template>
-      </div>
 
-      <div class="flex flex-row items-center justify-center">
-        <UBadge color="lime" variant="outline">NFT</UBadge>
-        <UButton
-          color="lime"
-          variant="link"
-          :to="hrefAirdrop"
-          :external="true"
-          target="_blank"
-        >
-          <img src="~/assets/token-icon.svg" class="w-5" />
-          <span>Token of Gratitude by Yanke Guo</span>
-        </UButton>
+        <div class="flex flex-row justify-center items-center">
+          <UBadge class="me-2" variant="outline" color="orange">{{
+            $t("donation")
+          }}</UBadge>
+          <template v-for="(itemGroup, idxGroup) in linksDonations">
+            <UButton
+              color="orange"
+              v-for="(item, idx) in itemGroup"
+              v-bind:key="idx"
+              size="sm"
+              variant="ghost"
+              :label="item.label"
+              :icon="item.icon"
+              :to="item.to"
+              target="_blank"
+            ></UButton>
+          </template>
+        </div>
+
+        <div class="flex flex-row items-center justify-center">
+          <UBadge color="lime" variant="outline">NFT</UBadge>
+          <UButton
+            color="lime"
+            variant="link"
+            :to="hrefAirdrop"
+            :external="true"
+            target="_blank"
+          >
+            <img src="~/assets/token-icon.svg" class="w-5" />
+            <span>Token of Gratitude by Yanke Guo</span>
+          </UButton>
+        </div>
       </div>
     </div>
 
